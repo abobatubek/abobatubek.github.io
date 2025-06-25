dcbb63e441755ddb8391f7118f2ae6fafe10f424
ElementoBorzi <113427648+ElementoBorzi@users.noreply.github.com>
2025-06-25 22:02:16 +0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -11,9 +11,13 @@ const videos = [
         videourl: "https://drive.google.com/file/d/1CLdp7MNAcOlTEGBE8dl328t2E1C9RKIs/preview",
         videoname: "MEMPHIS PHONK MIX"
     },
-        {
+    {
         videourl: "https://drive.google.com/file/d/1DRrzMJZ_vzsTlfxtghRV2Usr2KiBjHdS/preview",
         videoname: "1 HOUR AGGRESSIVE PHONK #4 _ Сборник агрессивного Фонка"
+    },
+    {
+        videourl: "https://drive.google.com/file/d/1HaKX5BVFMKoSEOu1kceAvWSq8svAEVdR/preview",
+        videoname: "БЕРЕМЕННА В 45 _ 3 СЕЗОН, 10 ВЫПУСК _ СТЭЛЛА, МОСКОВСКАЯ ОБЛАСТЬ"
     },
     // Добавьте новые видео здесь:
     // {
